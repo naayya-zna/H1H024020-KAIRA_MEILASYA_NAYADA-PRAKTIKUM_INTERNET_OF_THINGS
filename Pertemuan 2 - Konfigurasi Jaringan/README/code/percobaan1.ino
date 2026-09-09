915da98324aab6e9bd904a56fe9323660f7e10ec
nayya <nayeeuuc@gmail.com>
2026-09-09 20:35:26 +0700

    percobaan1.ino
    This Arduino sketch connects to a WiFi network and controls an LED based on the connection status.

---
+++ Pertemuan 2 - Konfigurasi Jaringan/README/code/percobaan1.ino
@@ -0,0 +1,42 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "naye";
+const char* password = "woylahbroo";
+
+const int ledPin = 4;  
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Menghubungkan ke WiFi:");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi berhasil terhubung!");
+  Serial.print("IP Address : ");
+  Serial.println(WiFi.localIP());
+  Serial.print("MAC Address : ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("RSSI (dBm) : ");
+  Serial.println(WiFi.RSSI());
+
+  digitalWrite(ledPin, HIGH);  
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Status: Terhubung");
+  } else {
+    Serial.println("Status: Terputus");
+    digitalWrite(ledPin, LOW);
+  }
+  delay(5000);
+}
